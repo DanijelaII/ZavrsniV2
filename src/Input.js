3bdec72b9fe9bc1061eb0ca0897195590c6a3fc8
DanijelaII <99415668+DanijelaII@users.noreply.github.com>
2022-12-30 16:58:23 +0100

    Add files via upload

---
+++ src/Input.js
@@ -0,0 +1,39 @@
+import {Component} from "react";
+import React from "react";
+
+class Input extends Component {
+  state = {
+    text: ""
+  }
+
+  onChange(e) {
+    this.setState({text: e.target.value});
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    this.setState({text: ""});
+    if(this.state.text===""){
+      alert("Prije slanja, unesite poruku!");
+    }else{
+      this.props.onSendMessage(this.state.text);}
+  }
+
+  render() {
+    return (
+      <div className="Input">
+        <form onSubmit={e => this.onSubmit(e)}>
+          <input
+            onChange={e => this.onChange(e)}
+            value={this.state.text}
+            type="text"
+            autoFocus={true}
+          />
+          <button></button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Input;
